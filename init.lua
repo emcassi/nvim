202daa20d0e436f032d31545395908b047c4354b
Alex Wayne <alex.wayne.dev@gmail.com>
2024-01-27 06:32:44 -0700

    added null lsp

--- init.lua
+++ init.lua
@@ -252,6 +252,8 @@ require("lazy").setup({
   },
   "github/copilot.vim",
 
+  "jose-elias-alvarez/null-ls.nvim",
+
   "andymass/vim-matchup",
   {
     "startup-nvim/startup.nvim",
@@ -564,8 +566,44 @@ local servers = {
   },
 }
 
+local null_ls = require("null-ls")
+
+local lsp_format = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
+local null_ls_on_attach = function(client, bufnr)
+	if not client.supports_method("textDocument/formatting") then
+		return
+	end
+
+	vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		group = augroup,
+		buffer = bufnr,
+		callback = function()
+			lsp_format(bufnr)
+		end,
+	})
+end
+
+null_ls.setup({
+	on_attach = null_ls_on_attach,
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.gdformat,
+		null_ls.builtins.formatting.prettier,
+		null_ls.builtins.formatting.black,
+	},
+})
 --
 -- Setup neovim lua configuration
 require("neodev").setup()
